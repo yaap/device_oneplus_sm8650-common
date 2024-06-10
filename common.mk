a34e17a1e3fcd4a487f03f0e77b1987608b2a5c8
chandu078 <chandudyavanapelli03@gmail.com>
2024-06-10 11:28:25 +0530

    sm8650-common: Forcefully copy fstab.qcom in the first stage ramdisk
    For some reason, it’s not copying into the first stage ramdisk with vendor_ramdisk_available: true
    
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- common.mk
+++ common.mk
@@ -252,9 +252,11 @@ PRODUCT_PACKAGES += \
     IPACM_Filter_cfg.xml
 
 # Init
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
+
 PRODUCT_PACKAGES += \
     fstab.qcom \
-    fstab.qcom.vendor_ramdisk \
     init.class_main.sh \
     init.oplus.rc \
     init.qcom.early_boot.sh \
