6523ab084d8f0b1e259ad8b9ce67908edab64e45
Giovanni Ricca <giovanniricca@proton.me>
2024-06-16 17:00:16 +0200

    sm8650-common: libshims: Add android::GraphicBufferMapper::unlock shim
    * Commit Ib4590b7a3b839a993f6c747c5f09a6312f7ab329 modified
      GraphicBufferMapper::unlock overload. Create a shim to provide the old
      symbol for blobs that need it
    
    Change-Id: I0e90ac0b383b05904df83076d990ad9335a40664

--- libshims/libshim_ui/GraphicBufferMapper.cpp
+++ libshims/libshim_ui/GraphicBufferMapper.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <stdint.h>
+#include <sync/sync.h>
 #include <ui/GraphicBufferMapper.h>
 #include <ui/Rect.h>
 #include <utils/Errors.h>
@@ -18,5 +19,17 @@ status_t _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPvP
         int32_t* /*outBytesPerPixel*/, int32_t* /*outBytesPerStride*/) {
     auto* gpm = static_cast<android::GraphicBufferMapper*>(thisptr);
     return gpm->lock(handle, usage, bounds, vaddr);
+}
+
+status_t _ZN7android19GraphicBufferMapper6unlockEPK13native_handle(void* thisptr,
+                                                                   buffer_handle_t handle) {
+    android::base::unique_fd outFence;
+    auto* gpm = static_cast<android::GraphicBufferMapper*>(thisptr);
+    status_t status = gpm->unlock(handle, &outFence);
+    if (status == android::OK && outFence.get() >= 0) {
+        sync_wait(outFence.get(), -1);
+        outFence.reset();
+    }
+    return status;
 }
 }
