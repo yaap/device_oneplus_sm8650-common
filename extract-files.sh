0eeafc5eed729172cebeb2777ae67e0daa383b39
Michael Bestas <mkbestas@lineageos.org>
2024-07-08 08:56:25 +0300

    sm8650-common: Remove media_codecs_google_c2*
    AOSP codecs can be used by default and it is not necessary to include
    them device side.
    
    Change-Id: I8227b06f0466b614f12fa9008402b92461ea3dd0

--- extract-files.sh
+++ extract-files.sh
@@ -84,7 +84,7 @@ function blob_fixup() {
             grep -q libshim_ui.so "$2" || "$PATCHELF" --add-needed libshim_ui.so "$2"
             ;;
         vendor/etc/media_codecs_pineapple.xml|vendor/etc/media_codecs_pineapple_vendor.xml)
-            sed -Ei "/media_codecs_(google_audio|google_telephony|google_video|vendor_audio)/d" "${2}"
+            sed -Ei "/media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio)/d" "${2}"
             ;;
     esac
 }
