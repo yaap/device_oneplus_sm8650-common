6ea8591777da040b5307918c04e2589d8338f7ef
Bruno Martins <bgcngm@gmail.com>
2024-08-22 00:03:01 +0100

    sm8650-common: Build QTI capabilityconfigstore interface
    Change-Id: Ib98304475c41eacbfac8845ab1b49244e2bf274a

--- common.mk
+++ common.mk
@@ -129,6 +129,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Configstore
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.capabilityconfigstore@1.0.vendor
+
 # Context Hub
 PRODUCT_PACKAGES += \
     android.hardware.contexthub-V2-ndk.vendor
