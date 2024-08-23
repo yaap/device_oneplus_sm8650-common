2f68b8d34fd7242bf542ef28b1226b09aaad4788
John Galt <johngaltfirstrun@gmail.com>
2024-08-23 09:09:58 -0400

    overlay-lineage->yaap: drop lineage-sdk, fix keyhandler

--- common.mk
+++ common.mk
@@ -372,7 +372,7 @@ $(call inherit-product, hardware/oplus/overlay/generic/generic.mk)
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-yaap
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
