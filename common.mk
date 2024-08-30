4e0c8a2cacd75636d12d61bbcb7f5dee2f545695
Pranav Vashi <neobuddy89@gmail.com>
2024-08-31 01:22:54 +0530

    sm8650-common: Kill media omx completely on 64-bit builds
    Change-Id: I259f61e1076e722d4f3d3fb112ad76f5fb1ed0a7
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -358,12 +358,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
-# OMX
-PRODUCT_PACKAGES += \
-    libOmxCore \
-    libmm-omxcore \
-    libstagefrighthw
-
 # OSENSE
 PRODUCT_PACKAGES += \
     vendor.oplus.hardware.osense.client-service
