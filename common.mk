48cb06a71afbd59a0ee998d5c4f5f0b848058087
John Galt <johngaltfirstrun@gmail.com>
2024-08-30 15:55:31 -0400

    Add OPlus Touch

--- common.mk
+++ common.mk
@@ -508,6 +508,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.mbms.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.mbms.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.oplus.hardware.touch
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.qti \
