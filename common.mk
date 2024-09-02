6f5a6615aa673ba74c8e2c802446c06690029881
John Galt <johngaltfirstrun@gmail.com>
2024-09-02 10:00:53 -0400

    configs: add display config from stock

--- common.mk
+++ common.mk
@@ -192,6 +192,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
+# DisplayConfig
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/display_id_4630946450791512195.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946450791512195.xml
+
 # Doze
 PRODUCT_PACKAGES += \
     OplusDoze
