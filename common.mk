0cba19c417313911abe21c11e077e16d676acfc6
John Galt <johngaltfirstrun@gmail.com>
2024-09-10 10:57:51 -0400

    ims: move to stock oplus ims
    There were some reported vowifi and verizon issues reported with bsp
    ims. Test this to see if they are resolved.

--- common.mk
+++ common.mk
@@ -368,6 +368,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.oplus.hardware.osense.client-service
 
+# Oplus-fwk
+$(call inherit-product, hardware/oplus/oplus-fwk/oplus-fwk.mk)
+
 # Overlays
 $(call inherit-product, hardware/oplus/overlay/generic/generic.mk)
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
