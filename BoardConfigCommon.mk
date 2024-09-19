40f2e881fc540946e4ecd0e1f556b93c72e4aadd
John Galt <johngaltfirstrun@gmail.com>
2024-09-19 10:41:36 -0400

    kernel: build from source

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -65,9 +65,8 @@ BOARD_RAMDISK_USE_LZ4 := true
 
 # DTB / DTBO
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-BOARD_KERNEL_SEPARATED_DTBO := true
-#BOARD_USES_QCOM_MERGE_DTBS_SCRIPT := true
-#TARGET_NEEDS_DTBOIMAGE := true
+BOARD_USES_QCOM_MERGE_DTBS_SCRIPT := true
+TARGET_NEEDS_DTBOIMAGE := true
 
 # Camera
 TARGET_CAMERA_PACKAGE_NAME := com.oplus.packageName
@@ -118,61 +117,58 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_IMAGE_NAME := Image
 
-KERNEL_LTO := none
-
-TARGET_FORCE_PREBUILT_KERNEL := true
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8650
 TARGET_KERNEL_CONFIG := \
     gki_defconfig \
-#    vendor/pineapple_GKI.config \
-#    vendor/oplus/pineapple_GKI.config \
-#    vendor/debugfs.config
+    vendor/pineapple_GKI.config \
+    vendor/oplus/pineapple_GKI.config \
+    vendor/debugfs.config
 
 # Kernel modules
-#BOARD_SYSTEM_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load.system_dlkm))
-#BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(COMMON_PATH)/modules.blocklist
-#BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load))
-#BOARD_VENDOR_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE)
-#BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load.vendor_boot))
-#BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load.recovery))
-#BOOT_KERNEL_MODULES := $(strip $(shell cat $(COMMON_PATH)/modules.load.recovery $(COMMON_PATH)/modules.include.vendor_ramdisk))
-#SYSTEM_KERNEL_MODULES := $(strip $(shell cat $(COMMON_PATH)/modules.include.system_dlkm))
+BOARD_SYSTEM_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load.system_dlkm))
+BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(COMMON_PATH)/modules.blocklist
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load))
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE)
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load.vendor_boot))
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load.recovery))
+BOOT_KERNEL_MODULES := $(strip $(shell cat $(COMMON_PATH)/modules.load.recovery $(COMMON_PATH)/modules.include.vendor_ramdisk))
+SYSTEM_KERNEL_MODULES := $(strip $(shell cat $(COMMON_PATH)/modules.include.system_dlkm))
 
-#TARGET_KERNEL_EXT_MODULE_ROOT := kernel/oneplus/sm8650-modules
-#TARGET_KERNEL_EXT_MODULES := \
-#	qcom/opensource/mmrm-driver \
-#	qcom/opensource/mm-drivers/hw_fence \
-#	qcom/opensource/mm-drivers/msm_ext_display \
-#	qcom/opensource/mm-drivers/sync_fence \
-#	qcom/opensource/securemsm-kernel \
-#	qcom/opensource/audio-kernel \
-#	qcom/opensource/camera-kernel \
-#	qcom/opensource/dataipa/drivers/platform/msm \
-#	qcom/opensource/datarmnet-ext/mem \
-#	qcom/opensource/datarmnet/core \
-#	qcom/opensource/datarmnet-ext/aps \
-#	qcom/opensource/datarmnet-ext/offload \
-#	qcom/opensource/datarmnet-ext/shs \
-#	qcom/opensource/datarmnet-ext/perf \
-#	qcom/opensource/datarmnet-ext/perf_tether \
-#	qcom/opensource/datarmnet-ext/sch \
-#	qcom/opensource/datarmnet-ext/wlan \
-#	qcom/opensource/display-drivers/msm \
-#	qcom/opensource/synx-kernel \
-#	qcom/opensource/dsp-kernel \
-#	qcom/opensource/eva-kernel \
-#	qcom/opensource/video-driver \
-#	qcom/opensource/graphics-kernel \
-#	qcom/opensource/wlan/platform \
-#	qcom/opensource/wlan/qcacld-3.0 \
-#	qcom/opensource/bt-kernel \
-#	qcom/opensource/spu-kernel \
-#	qcom/opensource/mm-sys-kernel/ubwcp \
-#	qcom/opensource/fingerprint \
-#	qcom/opensource/touch-drivers \
-#	nxp/opensource/driver \
-#	st/opensource/driver \
-#	st/opensource/eSE-driver
+TARGET_KERNEL_EXT_MODULE_ROOT := kernel/oneplus/sm8650-modules
+TARGET_KERNEL_EXT_MODULES := \
+	qcom/opensource/mmrm-driver \
+	qcom/opensource/mm-drivers/hw_fence \
+	qcom/opensource/mm-drivers/msm_ext_display \
+	qcom/opensource/mm-drivers/sync_fence \
+	qcom/opensource/securemsm-kernel \
+	qcom/opensource/audio-kernel \
+	qcom/opensource/camera-kernel \
+	qcom/opensource/dataipa/drivers/platform/msm \
+	qcom/opensource/datarmnet-ext/mem \
+	qcom/opensource/datarmnet/core \
+	qcom/opensource/datarmnet-ext/aps \
+	qcom/opensource/datarmnet-ext/offload \
+	qcom/opensource/datarmnet-ext/shs \
+	qcom/opensource/datarmnet-ext/perf \
+	qcom/opensource/datarmnet-ext/perf_tether \
+	qcom/opensource/datarmnet-ext/sch \
+	qcom/opensource/datarmnet-ext/wlan \
+	qcom/opensource/display-drivers/msm \
+	qcom/opensource/synx-kernel \
+	qcom/opensource/dsp-kernel \
+	qcom/opensource/eva-kernel \
+	qcom/opensource/video-driver \
+	qcom/opensource/graphics-kernel \
+	qcom/opensource/wlan/platform \
+	qcom/opensource/wlan/qcacld-3.0 \
+	qcom/opensource/bt-kernel \
+	qcom/opensource/spu-kernel \
+	qcom/opensource/mm-sys-kernel/ubwcp \
+	qcom/opensource/fingerprint \
+	qcom/opensource/touch-drivers \
+	nxp/opensource/driver \
+	st/opensource/driver \
+	st/opensource/eSE-driver
 
 # Lineage Health
 TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/oplus_chg/battery/mmi_charging_enable
