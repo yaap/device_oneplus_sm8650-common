92b4d7184e5e3bfdf4cc2e6bac0b915d8d8797dd
John Galt <johngaltfirstrun@gmail.com>
2024-10-02 14:42:49 -0400

    extract-files: fix qsap* seccomp failures

--- extract-files.sh
+++ extract-files.sh
@@ -102,7 +102,7 @@ function blob_fixup() {
             sed -i "/vp9.decoder/,/<\/MediaCodec>/d" "${2}"
             sed -i "/av1.decoder/,/<\/MediaCodec>/d" "${2}"
             ;;
-        vendor/etc/seccomp_policy/atfwd@2.0.policy|vendor/etc/seccomp_policy/wfdhdcphalservice.policy)
+        vendor/etc/seccomp_policy/atfwd@2.0.policy|vendor/etc/seccomp_policy/wfdhdcphalservice.policy|vendor/etc/seccomp_policy/qsap_sensors.policy|vendor/etc/seccomp_policy/gnss@2.0-qsap-location.policy)
             grep -q "gettid: 1" "${2}" || echo -e "\ngettid: 1" >> "${2}"
             ;;
         vendor/lib64/libqcodec2_core.so)
