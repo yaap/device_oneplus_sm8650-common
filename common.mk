28dd92b54549365809b091013e93a76ed6fea9a5
John Galt <johngaltfirstrun@gmail.com>
2024-10-08 22:29:34 -0400

    Move to A15 beta fingerprint
    V3->V4 is manageable, V1->V4 isn't

--- common.mk
+++ common.mk
@@ -218,7 +218,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint-service.oplus
+    android.hardware.biometrics.fingerprint-service.oplus \
+    android.hardware.biometrics.fingerprint-V3-ndk.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
