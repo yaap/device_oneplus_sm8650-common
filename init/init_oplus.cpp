5acddccd5763125f6bd14c25806c71e6023d449d
dianlujitao <dianlujitao@lineageos.org>
2025-08-23 01:20:06 +0000

    sm8650-common: init: Migrate to new regional properties override method
    Change-Id: I49f11a1377df34ba40b0a4e29e35b7eeda2207a8

--- init/init_oplus.cpp
+++ init/init_oplus.cpp
@@ -4,86 +4,56 @@
  */
 
 #include <android-base/logging.h>
+#include <android-base/parseint.h>
 #include <android-base/properties.h>
-
-#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
-#include <sys/_system_properties.h>
 
 #include <fs_mgr.h>
 
-#define NV_ID_US "161"
-#define NV_ID_IN "27"
-#define NV_ID_EU "68"
-#define NV_ID_CN "151"
+#include <string>
+#include <unordered_map>
 
-using android::base::GetProperty;
+namespace android {
+namespace init {
+uint32_t InitPropertySet(const std::string& name, const std::string& value);
+}  // namespace init
+}  // namespace android
+
+using android::base::ParseInt;
 using android::fs_mgr::GetKernelCmdline;
+using android::init::InitPropertySet;
 
-/*
- * Parse /proc/cmdline and extract value of given key.
- */
-std::string GetKernelCmdlineParam(const std::string& key) {
-    std::string value;
-    if (!GetKernelCmdline(key, &value)) {
-        LOG(ERROR) << key << " not found in /proc/cmdline";
-    }
-    return value;
-}
-
-/*
- * SetProperty does not allow updating read only properties and as a result
- * does not work for our use case. Write "OverrideProperty" to do practically
- * the same thing as "SetProperty" without this restriction.
- */
-void OverrideProperty(const char* name, const char* value) {
-    size_t valuelen = strlen(value);
-
-    prop_info* pi = (prop_info*)__system_property_find(name);
-    if (pi != nullptr) {
-        __system_property_update(pi, value, valuelen);
-    } else {
-        __system_property_add(name, strlen(name), value, valuelen);
-    }
-}
+namespace {
+constexpr std::string kCmdlineRegion = "oplus_region";
+const std::unordered_map<int, std::string> kRegionMap = {
+        {27, "IN"},
+        {68, "EU"},
+        {151, "CN"},
+        {161, "NA"},
+};
+}  // anonymous namespace
 
 /*
  * Only for read-only properties. Properties that can be wrote to more
  * than once should be set in a typical init script (e.g. init.oplus.hw.rc)
  * after the original property has been set.
  */
-void vendor_load_properties() {
-    auto bootmode = GetProperty("ro.bootmode", "");
-    auto hw_region_id = GetKernelCmdlineParam("oplus_region");
-    auto prjname = std::stoi(GetProperty("ro.boot.prjname", "0"));
-
-    // We do not want overrides in recovery
-    if (bootmode == "recovery")
-	    return;
-
-    if (hw_region_id == NV_ID_CN) {
-        OverrideProperty("ro.boot.hardware.revision", "CN");
-    } else if (hw_region_id == NV_ID_EU) {
-        OverrideProperty("ro.boot.hardware.revision", "EU");
-    } else if (hw_region_id == NV_ID_IN) {
-        OverrideProperty("ro.boot.hardware.revision", "IN");
-    } else if (hw_region_id == NV_ID_US) {
-        OverrideProperty("ro.boot.hardware.revision", "NA");
-    } else {
-        LOG(ERROR) << "Unexpected region ID: " << hw_region_id;
+void vendor_process_bootenv() {
+    std::string buf;
+    if (!GetKernelCmdline(kCmdlineRegion, &buf)) {
+        LOG(ERROR) << kCmdlineRegion << " not found in /proc/cmdline";
+        return;
     }
 
-    switch (prjname) {
-        case 22825:  // waffle CN
-            OverrideProperty("ro.product.device", "OP5929L1");
-            OverrideProperty("ro.product.vendor.device", "OP5929L1");
-            OverrideProperty("ro.product.product.model", "PJD110");
-            break;
-        case 22877:  // waffle ROW
-            OverrideProperty("ro.product.product.model",
-                    hw_region_id == NV_ID_IN ? "CPH2573" :
-                    hw_region_id == NV_ID_US ? "CPH2583" : "CPH2581");
-            break;
-        default:
-            LOG(ERROR) << "Unexpected prjname: " << prjname;
+    int region_id;
+    if (!ParseInt(buf, &region_id)) {
+        LOG(ERROR) << "Region ID [" << buf << "] is invalid";
+        return;
+    }
+
+    auto it = kRegionMap.find(region_id);
+    if (it == kRegionMap.end()) {
+        LOG(ERROR) << "Unexpected region ID: " << region_id;
+    } else {
+        InitPropertySet("ro.boot.hardware.revision", it->second);
     }
 }
