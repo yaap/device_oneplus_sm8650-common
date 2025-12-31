22d2267e167e1190c6f4e08333cf56b2c027383b
chandu078 <chandudyavanapelli03@gmail.com>
2025-12-31 06:14:11 +0000

    sm8650-common: Apply codec fixup to cliffs v0 variant
    Change-Id: I6443bf161646176ac5a8e6d0e1533bfe23017c88

--- extract-files.sh
+++ extract-files.sh
@@ -136,7 +136,7 @@ function blob_fixup() {
             sed -i "/zram or zsmalloc/d" "${2}"
             sed -i "s/-e \"zram\" -e \"zsmalloc\"//g" "${2}"
             ;;
-        vendor/etc/media_codecs_pineapple.xml|vendor/etc/media_codecs_pineapple_vendor.xml)
+        vendor/etc/media_codecs_pineapple.xml|vendor/etc/media_codecs_pineapple_vendor.xml|vendor/etc/media_codecs_cliffs_v0.xml)
             [ "$2" = "" ] && return 0
             sed -Ei "/media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio)/d" "${2}"
             ;;
